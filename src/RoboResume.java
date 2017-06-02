8e839ec0af89c86dc856e7c8ffec27b1fd7aef73
owolfhu1 <owolfhu1@gmail.com>
2017-06-02 02:43:45 -0400

    Update RoboResume.java

--- src/RoboResume.java
+++ src/RoboResume.java
@@ -4,119 +4,141 @@ import java.util.Scanner;
 
 public class RoboResume {
 
-	public static void main(String args[]){
-		Scanner scan = new Scanner(System.in);
-		String name = null;
-		String email = null;
-		List<String> education = new ArrayList<String>();
-		List<String> work = new ArrayList<String>();
-		List<String> skills = new ArrayList<String>();
-		
-		System.out.print("Please enter your name: ");
-		do {
-			
-			name = scan.nextLine();
-			if (name.length() < 3 || name.length() > 12) {
-				
-				System.out.print("Please enter a name (3 to 12 char): ");
-				name = null;
-			}
-			
-		} while (name == null);
-		
-		System.out.printf("Hello %s, please enter your email: ", name);
-		
-		do {
-			
-			email = scan.nextLine();
-			if(email.indexOf('@') == -1 || email.indexOf('.') == -1) {
-				
-				System.out.print("Please enter a valid email to continue: ");
-				email = null;
-			}
-			
-		} while (email == null);
-		
-		System.out.printf("Ok %s, with email: %s, lets enter some education (enter a blank line to move onto work history):\n", name, email);
-		
-		for (int i = 0; i < 10; i++) {
-			String temp = scan.nextLine();
-			if (temp.equals("")) {
-				if (education.isEmpty()) {
-					System.out.println("You must enter at least one education field: ");
-					i = 0;
-				} else {
-					i = 10;
-				}
-			} else {
-				education.add(temp);
-			}
-		}
-		
-		System.out.println("you have entered: ");
-		for (String edu : education) {
-			System.out.println(edu);
-		}
-		System.out.println("Now enter your work history(blank line to move on to skills): ");
-		
-		for (int i = 0; i < 10; i++) {
-			String temp = scan.nextLine();
-			if (temp.equals("")) {
-				i = 10;
-			} else {
-				work.add(temp);
-			}
-		}
-		
-		System.out.println("you have entered: ");
-		for (String w : work) {
-			System.out.println(w);
-		}
-		
-		System.out.println("Now you may enter your skills (again, blank line to move on): ");
-		
-		for (int i = 0; i < 20; i++){
-			String temp = scan.nextLine();
-			if (temp.equals("")) {
-				if (skills.isEmpty()) {
-					System.out.println("You must enter at least one skill: ");
-					i = 0;
-				} else {
-					i = 20;
-				}
-			} else {
-				skills.add(temp);
-			}
-		}
-		
-		System.out.println("you have entered: ");
-		for (String skill : skills) {
-			System.out.println(skill);
-		}
-		
-		System.out.println("Press return to print out resume.");
-		scan.nextLine();
-		System.out.println("=============================================================");
-		System.out.println(name);
-		System.out.printf("%s \n\n", email);
-		System.out.println("EDUCATION:");
-		for (String edu : education) {
-			System.out.printf("%s \n\n", edu);
-		}
-		if (!work.isEmpty()){
-			System.out.println("WORK:");
-			for (String w : work) {
-				System.out.printf("%s \n\n", w);
-			}
-		}
-		System.out.println("SKILLS:");
-		for (String skill : skills) {
-			System.out.println(skill);
-		}
-		System.out.println("=============================================================");
-		
-		scan.close();
-	}
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        String name;
+        String email;
+        List<String[]> education = new ArrayList<>();
+        List<Object[]> work = new ArrayList<>();
+        List<String[]> skills = new ArrayList<>();
+
+        System.out.print("Please enter your name: ");
+        do {
+            name = in.nextLine();
+            if (name.length() < 3 || name.length() > 20) {
+                out("Please enter a name (3 to 20 char): ");
+                name = null;
+            }
+
+        } while (name == null);
+
+        out("Hello %s, please enter your email: ", name);
+        do {
+            email = in.nextLine();
+            if(email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+                System.out.print("Please enter a valid email to continue: ");
+                email = null;
+            }
+        } while (email == null);
+
+        out("Ok %s, with email: %s, lets enter some education (leave all fields blank to move onto work experience):\n", name, email);
+
+        for (int i = 0; i < 10; i++) {
+            int SCHOOL = 0, DEGREE = 1;
+            String temp[] = new String[2];
+
+            out("Please enter a school: ");
+            temp[SCHOOL] = in.nextLine();
+
+            out("Please enter the degree you got at %s: ", temp[SCHOOL]);
+            temp[DEGREE] = in.nextLine();
+
+            if (temp[SCHOOL].equals("") && temp[DEGREE].equals("")) {
+                if (education.isEmpty()) {
+                    out("You must enter at least one educational achievement or you will look like a deadbeat...");
+                } else i = 10;
+            } else education.add(temp);
+        }
+
+        out("Alright %s, its time to enter some work experience (leave all fields blank to move onto skills):\n", name);
+
+
+        for (int i = 0; i < 10; i++) {
+            int COMPANY = 0, TITLE = 1, TASKS = 2;
+            Object[] temp = { null, null, null };
+            List<String> tempList = new ArrayList<>();
+            out("Please enter the company: ");
+            temp[COMPANY] = in.nextLine();
+
+            out("Please enter your job title at %s: ", temp[COMPANY]);
+            temp[TITLE] = in.nextLine();
+
+            for (int x = 0; x < 10; x++) {
+                String t;
+                out("Task: ");
+                t = in.nextLine();
+                if (t.isEmpty()) x = 10;
+                else tempList.add(t);
+            }
+            if (!tempList.isEmpty()) temp[TASKS] = tempList;
+
+            if (temp[COMPANY].equals("") && temp[TITLE].equals("") && temp[TASKS] == null) i = 10;
+            else work.add(temp);
+        }
+
+        out("Ok %s, you have added %d schools and %d jobs, time to add some skills: \n", name, education.size(), work.size());
+
+        for (int i = 0; i < 20; i++) {
+            int SKILL = 0, LEVEL = 1;
+            String temp[] = new String[2];
+
+            out("Skill: ");
+            temp[SKILL] = in.nextLine();
+
+            out("Proficiency at %s: ", temp[SKILL]);
+            temp[LEVEL] = in.nextLine();
+
+            if (temp[SKILL].equals("") && temp[LEVEL].equals("")) {
+                if (skills.isEmpty()) {
+                    out("You must enter at least one skill or they will think you can't do anything.");
+                } else i = 20;
+            } else skills.add(temp);
+
+        }
+
+        out("Alright thats it! Press enter to print your resume...");
+        in.nextLine();
+        in.close();
+        
+        out("\n============================================================");
+        out("\n============================================================\n\n");
+
+
+        out("%s\n%s\n\nEDUCATION:\n", name, email);
+
+        for (String[] array : education) {
+            int SCHOOL = 0, DEGREE = 1;
+            out("\n%s,\n%s\n", array[SCHOOL], array[DEGREE]);
+        }
+
+        if (!work.isEmpty()) {
+            int COMPANY = 0, TITLE = 1, TASKS = 2;
+            out("\nWORK EXPERIENCE:\n");
+            for (Object[] array : work) {
+                out("\n%s,\n%s\n", array[TITLE], array[COMPANY]);
+                if (array[TASKS] != null) {
+                    for (String task : (List<String>)array[TASKS]) {
+                        out("-%s\n", task);
+                    }
+                }
+            }
+        }
+
+        out("\nSKILLS:\n\n");
+
+        for (String[] array : skills) {
+            int SKILL = 0, LEVEL = 1;
+            out("-%s, proficiency: %s \n", array[SKILL], array[LEVEL]);
+        }
+
+        out("\n============================================================");
+        out("\n============================================================\n");
+	    
+    }
+
+    public static void out(String string, Object... variables) {
+        System.out.printf(string, variables);
+    }
 	
 }
 
